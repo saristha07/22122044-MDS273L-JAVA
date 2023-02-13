054b1f254b67a6b077e28b0bc75bfeb7d6eb6473
saristha07 <saristha.saha@msds.christuniversity.in>
2023-02-13 10:37:22 +0530

    Lab3

--- Lab3/Lab3.java
+++ Lab3/Lab3.java
@@ -1,86 +1,81 @@
 import java.util.Scanner;
-public class Lab3{
-    Scanner scan= new Scanner(System.in);
-    public static String arr[][]=new String[2][5];
 
-    public static void entry(){
-        for(int i=1;i<3;i++){
-            System.out.println("Please enter the name of person"+i+": ");
-            String name=scan.nextLine();
-            System.out.println("Please enter the registration number of person"+i+": ");
-            String reg=scan.nextLine();
-            System.out.println("Please enter the email of person"+i+": ");
-            String email=scan.nextLine();
-            System.out.println("Please enter the class of person"+i+": ");
-            String cls=scan.nextLine();
-            System.out.println("Please enter the department of person"+i+": ");
-            String dept=scan.nextLine();
-            if(i==1){
-                arr[0][0]=name;
-                arr[0][1]=reg;
-                arr[0][2]=email;
-                arr[0][3]=cls;
-                arr[0][4]=dept;
-            }
-            if(i==2){
-                arr[1][0]=name;
-                arr[1][1]=reg;
-                arr[1][2]=email;
-                arr[1][3]=cls;
-                arr[1][4]=dept;
-            }
-        }
+public class Lab3 {
+  static String[] names = new String[1024];
+  static String[][] details = new String[1024][5];
+  static int count = 0;
+
+  // Function to collect the details of a student
+  public static void collectDetails() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the details of the student:");
+    System.out.print("Name: ");
+    String name = sc.nextLine();
+    if (searchName(name) != -1) {
+      System.out.println("Details of this student are already entered!");
+      return;
     }
-
-    public static void display(){
-        System.out.println("The entered array is: ");
-        for(int i=0;i<2;i++){
-            for(int j=0;j<5;j++){
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
+    names[count] = name;
+    System.out.print("Register Number: ");
+    details[count][1] = sc.nextLine();
+    System.out.print("Email: ");
+    details[count][2] = sc.nextLine();
+    System.out.print("Class: ");
+    details[count][3] = sc.nextLine();
+    System.out.print("Department: ");
+    details[count][4] = sc.nextLine();
+    count++;
+  }
+// Function to display the details of a student
+  public static void displayDetails() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the name of the student: ");
+    String name = sc.nextLine();
+    int index = searchName(name);
+    if (index == -1) {
+      System.out.println("Details of this student are not found!");
+      return;
     }
+    System.out.println("Details of the student:");
+    System.out.println("Name: " + names[index]);
+    System.out.println("Register Number: " + details[index][1]);
+    System.out.println("Email: " + details[index][2]);
+    System.out.println("Class: " + details[index][3]);
+    System.out.println("Department: " + details[index][4]);
+  }
 
-    public static void search(){
-        int p=0;
-        System.out.println("Enter the name to be searched: ");
-        String searchName=scan.nextLine();
-        if(searchName.equalsIgnoreCase(arr[0][0])){
-            p=0;
-            System.out.println("The name has been found!!");
-        }
-        else if(searchName.equalsIgnoreCase(arr[1][0])){
-            p=1;
-            System.out.println("The name has been found!!");
-        }
-        System.out.println("The details of the searched name: ");
-        for(int i=0;i<5;i++){
-            System.out.print(arr[p][i]+" ");
-        }
+  // Function to search the name in the list of students
+  public static int searchName(String name) {
+    for (int i = 0; i < count; i++) {
+      if (names[i].equals(name)) {
+        return i;
+      }
     }
-
-    public static void main(String[]args){ 
-        
-        System.out.println("Some choices are given below:\n1. Collect the details of the student.\n2. Display the details of the student.\n3. Search the details of the student.");
-        System.out.println("Please enter your choice: ");
-        int ch=Integer.parseInt(scan.nextLine());
-
-        switch(ch){
-            case 1:
-                System.out.println("Your choice is 1.");
-                entry();
-                break;
-            
-            case 2:
-                System.out.println("Your choice is 2.");
-                display();
-                break;
-
-            case 3:
-                System.out.println("Your choice is 3.");
-                search();
-                break;        
-        }
+    return -1;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      System.out.println("Menu:");
+      System.out.println("1. Enter the details of a student");
+      System.out.println("2. Display the details of a student");
+      System.out.println("3. Exit");
+      System.out.print("Enter your choice: ");
+      int choice = sc.nextInt();
+      switch (choice) {
+        case 1:
+          collectDetails();
+          break;
+        case 2:
+          displayDetails();
+          break;
+        case 3:
+          System.exit(0);
+          break;
+        default:
+          System.out.println("Invalid choice! Try again.");
+          break;
+     }
     }
+ }
 }
